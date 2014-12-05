9043f693fa9398fd7bbefe5b66cc3b1d7ee4d626
Nino M <coffee@rubyengineer.com>
2014-12-05 20:49:35 +0100

    Clean-up specs

--- html_generator_specs.rb
+++ html_generator_specs.rb
@@ -8,10 +8,6 @@ describe RedBubble do
     expect(subject.const_get('VERSION')).not_to be_empty
   end
 
-  let(:file_path) { File.expand_path(File.join(File.dirname(__FILE__), 'works.xml')) }
-  let(:output_path) { File.expand_path(File.join(File.dirname(__FILE__), 'static_html')) }
-  let(:html_generator) { RedBubble::HtmlGenerator.new(file_path, output_path) }
-
   before(:each) do
     Object.send(:remove_const, 'RedBubble')
     load 'html_generator.rb'
@@ -179,26 +175,6 @@ describe RedBubble do
         let(:model) { RedBubble::ModelHtmlUnit.new 'NIKON CORPORATION', 'NIKON D80' }
 
         specify { expect(model.title).to eq('NIKON CORPORATION | NIKON D80') }
-      end
-
-      context 'with file example' do
-        let!(:html_generator) { RedBubble::HtmlGenerator.new(file_path, output_path) }
-        let!(:make) { RedBubble::MakeHtmlUnitCollection.units.first }
-        let!(:model) { make.models.first }
-
-        specify { expect(model.title).to eq('NIKON CORPORATION | NIKON D80') }
-      end
-    end
-
-    describe '#navigation' do
-      context 'with file example' do
-        let!(:html_generator) { RedBubble::HtmlGenerator.new(file_path, output_path) }
-        let!(:make) { RedBubble::MakeHtmlUnitCollection.units.first }
-        let!(:model) { make.models.first }
-
-        specify { expect(model.navigation.size).to eq(2) }
-        specify { expect(model.navigation[1].title).to eq('NIKON CORPORATION') }
-        specify { expect(model.navigation[1].filename).to eq('make_nikon_corporation.html') }
       end
     end
   end
