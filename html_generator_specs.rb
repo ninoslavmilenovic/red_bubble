7c98b3b25b0ea6d52eb9116207cc4db890982dc7
Nino M <coffee@rubyengineer.com>
2014-12-07 18:35:39 +0100

    Add more specs

--- html_generator_specs.rb
+++ html_generator_specs.rb
@@ -13,6 +13,7 @@ describe RedBubble do
     load 'html_generator.rb'
   end
 
+
   describe RedBubble::URL do
     describe '#initialize' do
       context 'when valid arguments' do
@@ -145,6 +146,25 @@ describe RedBubble do
     end
   end
 
+
+  describe RedBubble::HtmlUnit do
+    describe '#generate_filename' do
+      before do 
+        class UnitHost < Struct.new :base
+          include RedBubble::HtmlUnit
+
+          def filename
+            generate_filename(base)
+          end
+        end
+        @unit = UnitHost.new('NIKON Corporation "D7100')
+      end
+
+      specify { expect(@unit.filename).to eq('nikon_corporation_d7100.html') }
+    end
+  end
+
+
   describe RedBubble::ModelHtmlUnit do
     describe '#make' do
       context 'when valid arguments' do
@@ -175,6 +195,241 @@ describe RedBubble do
         let(:model) { RedBubble::ModelHtmlUnit.new 'NIKON CORPORATION', 'NIKON D80' }
 
         specify { expect(model.title).to eq('NIKON CORPORATION | NIKON D80') }
+      end
+    end
+
+    describe '#navigation' do
+      context 'when valid arguments' do
+        let!(:make) { RedBubble::MakeHtmlUnit.new 'NIKON CORPORATION' }
+        let!(:make_collection) { RedBubble::MakeHtmlUnitCollection }
+        let!(:model) { RedBubble::ModelHtmlUnit.new make.make, 'NIKON D80' }
+
+        before { make_collection.units << make }
+
+        specify do 
+          expect(model.navigation).to match_array([
+            RedBubble::IndexHtmlUnit.new.link, 
+            make_collection.link_by_make(make.make)
+          ])
+        end
+
+        specify { expect(model.navigation[0].title).to eq('Index') }
+        specify { expect(model.navigation[0].filename).to eq('index.html') }
+        specify { expect(model.navigation[1].title).to eq(make.title) }
+        specify { expect(model.navigation[1].filename).to eq(make.filename) }
+      end
+    end
+
+    describe '#thumbnails' do
+      context 'when valid arguments' do
+        let!(:make) { RedBubble::MakeHtmlUnit.new 'NIKON CORPORATION' }
+        let!(:make_collection) { RedBubble::MakeHtmlUnitCollection }
+        let!(:model) { RedBubble::ModelHtmlUnit.new make.make, 'NIKON D80' }
+
+        before { make_collection.units << make }
+
+        context 'when no images are present' do
+          specify do
+            expect(model.thumbnails).to match_array([])
+          end
+        end
+
+        context 'when images are present' do
+          let!(:work1) do
+            {
+              "filename"=>{"$"=>"1620421.jpg"}, 
+              "urls"=> {
+                "url"=>[
+                  {"@type"=>"small", "$"=>"http://ih1.redbubble.net/work.31820.1.flat,135x135,075,f.jpg"},
+                  {"@type"=>"medium", "$"=>"http://ih1.redbubble.net/work.31820.1.flat,300x300,075,f.jpg"},
+                  {"@type"=>"large", "$"=>"http://ih1.redbubble.net/work.31820.1.flat,550x550,075,f.jpg"}
+                ]
+              },
+              "exif"=> {
+                "model"=>{"$"=>"NIKON D80"},
+                "make"=>{"$"=>"NIKON CORPORATION"}
+              }
+            }
+          end
+          let!(:image1) { RedBubble::Image.new(work1) }
+          let!(:image_collection) { RedBubble::ImageCollection }
+
+          before { image_collection.images << image1 }
+          
+          specify do
+            expect(model.thumbnails).to match_array(RedBubble::ImageCollection.by_make_model(model.make, model.model))
+          end
+          specify { expect(model.thumbnails.size).to eq(1) }
+          specify { expect(model.thumbnails).to match_array([image1]) }
+        end
+      end
+    end
+  end
+
+
+  describe RedBubble::ModelHtmlUnitCollection do
+    let(:unit1) { RedBubble::ModelHtmlUnit.new 'NIKON CORPORATION', 'NIKON D80' }
+    let(:unit2) { RedBubble::ModelHtmlUnit.new 'Canon', 'Canon 5D III' }
+    let(:collection) { RedBubble::ModelHtmlUnitCollection }
+
+    before do
+      collection.units << unit1
+      collection.units << unit2
+    end
+
+    describe '#units' do
+      specify { expect(collection.units).to match_array([unit1, unit2]) }
+    end
+
+    describe '#by_make' do
+      specify { expect(collection.by_make(unit1.make)).to match_array([unit1]) }
+    end
+
+    describe '#links_by_make' do
+      specify do 
+        expect(collection.links_by_make(unit2.make)).to \
+          match_array([RedBubble::Link.new(unit2.title, unit2.filename)])
+      end
+    end
+  end
+
+
+  describe RedBubble::MakeHtmlUnit do
+    describe '#make' do
+      context 'when valid arguments' do
+        let(:make) { RedBubble::MakeHtmlUnit.new 'NIKON CORPORATION' }
+
+        specify { expect(make.make).to eq('NIKON CORPORATION')  }
+      end
+    end
+
+    describe '#filename' do
+      context 'when valid arguments' do
+        let(:make) { RedBubble::MakeHtmlUnit.new 'NIKON CORPORATION' }
+
+        specify { expect(make.filename).to eq('make_nikon_corporation.html') }
+      end
+    end
+
+    describe '#title' do
+      context 'when valid arguments' do
+        let(:make) { RedBubble::MakeHtmlUnit.new 'NIKON CORPORATION' }
+
+        specify { expect(make.title).to eq('NIKON CORPORATION') }
+      end
+    end
+
+    describe '#navigation' do
+      context 'when valid arguments' do
+        let!(:make) { RedBubble::MakeHtmlUnit.new 'NIKON CORPORATION' }
+        let!(:model) { RedBubble::ModelHtmlUnit.new make.make, 'NIKON D80' }
+        let!(:model_collection) { RedBubble::ModelHtmlUnitCollection }
+
+        before { model_collection.units << model }
+
+        specify do 
+          expect(make.navigation).to match_array([RedBubble::IndexHtmlUnit.new.link] \
+            + model_collection.links_by_make(make.make)
+          )
+        end
+
+        specify { expect(make.navigation[0].title).to eq('Index') }
+        specify { expect(make.navigation[0].filename).to eq('index.html') }
+        specify { expect(make.navigation[1].title).to eq(model.title) }
+        specify { expect(make.navigation[1].filename).to eq(model.filename) }
+      end
+    end
+
+    describe '#thumbnails' do
+      context 'when valid arguments' do
+        let!(:make) { RedBubble::MakeHtmlUnit.new 'NIKON CORPORATION' }
+        let!(:make_collection) { RedBubble::MakeHtmlUnitCollection }
+
+        before { make_collection.units << make }
+
+        context 'when no images are present' do
+          specify do
+            expect(make.thumbnails).to match_array([])
+          end
+        end
+
+        context 'when images are present' do
+          let!(:work1) do
+            {
+              "filename"=>{"$"=>"1620421.jpg"}, 
+              "urls"=> {
+                "url"=>[
+                  {"@type"=>"small", "$"=>"http://ih1.redbubble.net/work.31820.1.flat,135x135,075,f.jpg"},
+                  {"@type"=>"medium", "$"=>"http://ih1.redbubble.net/work.31820.1.flat,300x300,075,f.jpg"},
+                  {"@type"=>"large", "$"=>"http://ih1.redbubble.net/work.31820.1.flat,550x550,075,f.jpg"}
+                ]
+              },
+              "exif"=> {
+                "model"=>{"$"=>"NIKON D80"},
+                "make"=>{"$"=>"NIKON CORPORATION"}
+              }
+            }
+          end
+          let!(:image1) { RedBubble::Image.new(work1) }
+          let!(:image_collection) { RedBubble::ImageCollection }
+
+          before { image_collection.images << image1 }
+          
+          specify do
+            expect(make.thumbnails).to match_array(RedBubble::ImageCollection.by_make_top(make.make, 10))
+          end
+          specify { expect(make.thumbnails.size).to eq(1) }
+          specify { expect(make.thumbnails).to match_array([image1]) }
+        end
+      end
+    end
+
+    describe '#models' do
+      context 'when valid arguments' do
+        let!(:make) { RedBubble::MakeHtmlUnit.new 'NIKON CORPORATION' }
+        let!(:model1) { RedBubble::ModelHtmlUnit.new make.make, 'NIKON D80' }
+        let!(:model2) { RedBubble::ModelHtmlUnit.new make.make, 'NIKON D3100' }
+        let!(:collection) { RedBubble::ModelHtmlUnitCollection }
+
+         before do
+          collection.units << model1
+          collection.units << model2
+         end
+
+        specify { expect(make.models).to match_array([model1, model2]) }
+      end
+    end
+  end
+
+
+  describe RedBubble::MakeHtmlUnitCollection do
+    let(:unit1) { RedBubble::MakeHtmlUnit.new 'NIKON CORPORATION' }
+    let(:unit2) { RedBubble::MakeHtmlUnit.new 'Canon' }
+    let(:collection) { RedBubble::MakeHtmlUnitCollection }
+
+    before do
+      collection.units << unit1
+      collection.units << unit2
+    end
+
+    describe '#units' do
+      specify { expect(collection.units).to match_array([unit1, unit2]) }
+    end
+
+    describe '#links' do
+      specify do 
+        expect(collection.links).to \
+          match_array([
+            RedBubble::Link.new(unit1.title, unit1.filename), 
+            RedBubble::Link.new(unit2.title, unit2.filename)
+          ])
+      end
+    end
+
+    describe '#link_by_make' do
+      specify do 
+        expect(collection.link_by_make(unit2.make)).to \
+          eq(RedBubble::Link.new(unit2.title, unit2.filename))
       end
     end
   end
